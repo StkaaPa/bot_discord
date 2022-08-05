f64f39bf6ec3c9a999ca9a0dd6ed3405810b30bf
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-08-05 01:16:57 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Require the necessary discord.js classes
+const {Client, GatewayIntentBits} = require("discord.js")
+const config = require("./config.json")
+
+// Create a new client (our bot) instance
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.on("ready", () => {
+  console.log('Ready!');
+  client.user.setActivity("I´m BOT!");
+})
+
+// check if an interaction is a chat input command
+client.on('interactionCreate', async interaction => {
+  if(!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if(commandName === 'ping'){
+    await interaction.reply('pong!');
+  } else if (commandName === 'server'){
+    await interaction.reply(`Server name: ${interaction.guild.name}\n Total members: ${interaction.guild.memberCount}`);
+  } else if (commandName === 'user') {
+    await interaction.reply(`Your Tag: ${interaction.user.tag}}`)
+  }
+});
+
+// Login to Discord with your client's token
+client.login(config.BOT_TOKEN);
