99ac007f0f7ac39b1ad86ff4de3611250fce589c
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-08-09 17:36:18 +0100

    update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,11 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, messageLink } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('Pong!')
+    const msg = await interaction.reply({content: 'Pong!', fetchReply: true});
+    await interaction.editReply({content:`Bot Ping: ${msg.createdTimestamp - interaction.createdTimestamp}ms.`})
   }
 }
