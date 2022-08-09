0c0d2c0dd1bbec23586e5542fdd7e7a0ab916c54
Samuel gonçalves <goncalvesamuel99@gmail.com>
2022-08-09 17:36:29 +0100

    update deploy-commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,28 +1,37 @@
-const { SlashCommandBuilder, Routes } = require("discord.js");
-const { REST } = require('@discordjs/rest');
-const { clientId, guildId, BOT_TOKEN } = require('./config.json');
+//! WHENEVER YOU WANT TO REGISTER A COMMAND FOR THE BOT, YOU ALWAYS HAVE TO RUN THIS .JS FILE FIRST
 
-const commands = [
-  new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-  new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
-  new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
-]
+const fs = require("node:fs");
+const path = require("node:path");
+const { Routes } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { clientId, guildId, BOT_TOKEN } = require("./config.json");
 
-  .map(command => command.toJSON());
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandsFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
-  const rest = new REST({version: '10'}).setToken(BOT_TOKEN)
+for (const file of commandsFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
+}
 
-  rest.put(Routes.applicationGuildCommands(clientId, guildId), {body: commands})
-      .then(() => console.log('Successfully registered application commands!'))
-      .catch(console.error)
+const rest = new REST({ version: "10" }).setToken(BOT_TOKEN);
 
-  /* to delete guild-based commands
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands!"))
+  .catch(console.error);
+
+/* to delete guild-based commands
   rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
       .then(() => console.log('Successfully deleted guild command!'))
       .catch(console.error);
    */
 
-  /* to delete all guild-based commands at the same time
+/* to delete all guild-based commands at the same time
   rest.delete(Routes.applicationGuildCommand(clientId, guildId), {body: []})
       .then(() => console.log('Successfully deleted all guild commands!'))
       .catch(console.error);
